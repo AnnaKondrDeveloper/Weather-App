077488e75c3f9b64f215d8ab68d5f9c3704b0897
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2022-12-26 14:34:45 -0500

    Creat app with API

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
 const apiKey = "1b0e9e90c06f650a8d835b90a1849910";
 
-const main = document.getElementById("main")
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) => "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1b0e9e90c06f650a8d835b90a1849910&units=metric#";
+
+async function getWeatherByLocation(city) {
+	const resp = await fetch(url(city), {
+		origin : "cros",
+	});
+	const respData = await resp.json();
+	console.log(respData);
+	addWeatherToPage(respData);
+};
+
+function addWeatherToPage(data){
+	const temp = Math.floor(data.main.temp);
+
+	const weather = document.createElement('div')
+	weather.classList.add('weather');
+
+	weather.innerHTML = 	`	
+	<h3>${data.name}</h3>
+	<h2>
+	<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> 
+	${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+	</h2>
+	<small>${data.weather[0].main}</small>`;
+
+ //   cleanup 
+	main.innerHTML= "";
+	 main.appendChild(weather);
+};
+
+form.addEventListener('submit',(e) =>{
+ e.preventDefault();
+
+ let city = search.value;
+
+ if(city){
+	getWeatherByLocation(city)
+ }
+
+});
